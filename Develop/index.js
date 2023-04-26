4afdf4461c84ffae34ff39ac3c59db7dd5c20cb1
josik <joe.code.mckinney@gmail.com>
2023-04-26 15:26:16 -0500

    added variables to reference packages required and created list of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,67 @@
-// TODO: Include packages needed for this application
+//* Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//* Create an array of questions for user input
+const questions = [
+  {
+    //project title
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of the README.md file?'
+  },
+  {
+    //description
+    type: 'input',
+    name: 'description',
+    message: 'What is the despription of the README.md file?'
+  },
+  {
+    //installation instructions
+    type: 'input',
+    name: 'installation',
+    message: 'What is the installation instructions of the README.md file?'
+  },
+  {
+    //usage information
+    type: 'input',
+    name: 'usage',
+    message: 'What is the usage information of the README.md file?'
+  },
+  {
+    //contribution guidelines
+    type: 'input',
+    name: 'contribution',
+    message: 'What is the contribution guidelines of the README.md file?'
+  },
+  {
+    //test instructions
+    type: 'input',
+    name: 'tests',
+    message: 'What is the test instructions of the README.md file?'
+  },
+  {
+    // WHEN I enter my email address
+    
+    type: 'email',
+    name: 'email',
+    message: 'What is your email address if someone has questions?'
+  },
+  {
+    //License selector
+    type: 'checkbox',
+    name: 'license',
+    message: 'What license does the project use?'
+  },
+  {
+    //GitHub username
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
+  }
+ 
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
