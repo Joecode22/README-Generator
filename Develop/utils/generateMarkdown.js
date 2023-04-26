ffa9a241e4a51d89098fb2b3d2995096022b0e14
josik <joe.code.mckinney@gmail.com>
2023-04-26 18:15:29 -0500

    added table of contents to the markdown generator function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -25,6 +25,14 @@ ${renderLicenseBadge(data.license)}
 
 ## Table of Contents
 
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#Tests)
+7. [Questions](#questions)
+
 ## Installation
 
 To install necessary dependencies, run the following command: 
